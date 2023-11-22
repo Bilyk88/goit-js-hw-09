5b6dfe0474294ba9088029a976e9f761ac5410c2
Iryna Bilyk <bilykirochka@gmail.com>
2023-11-22 23:19:16 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,48 +2,54 @@ const btnCreatePromises = document.querySelector("button");
 const delayInput = document.querySelector('input[name="delay"]');
 const delayStepInput = document.querySelector('input[name="step"]');
 const amountInput = document.querySelector('input[name="amount"]');
-console.log(delayStepInput);
 
 btnCreatePromises.addEventListener('click', createPromise);
 
 function createPromise(position, delay) {
 
-  return promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
+  return new Promise((resolve, reject) => {
+
     const shouldResolve = Math.random() > 0.3;
 
+    setTimeout(() => {
+    
     if (shouldResolve) {
-      resolve({ position, delay });
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
     } else {
-      reject({ position, delay });
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
     }
   }, delay);
   });
   
 };
 
-function handleSubmit() {
-  
-  const amount = parseInt(amountInput.value);
-  const delay = parseInt(delayInput.value);
-  const step = parseInt(delayStepInput.value);
 
+const delay = delayInput.value;
+const step = Number(delayStepInput.value);
+const amount = Number(amountInput.value);
+console.log(delay);
+  
   for (let i = 0; i < amount; i += 1) {
-    const position = i + 1;
-    const currentDelay = delay + i * step;
-                
-    createPromise(position, currentDelay);
+    const  currentPosition = i + 1;
+    const currentDelay = delay + (i - 1) * step;
+                    
+    createPromise(currentPosition, currentDelay).then(result => {
+    console.log(result);
+  })
+  .catch(result => {
+    console.log(result);
+  })
   }
-};
+
 
    
 
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// createPromise(position, currentDelay)
+//   .then(result => {
+//     console.log(`✅ Fulfilled promise ${result.position} in ${result.currentDelay}ms`);
+//   })
+//   .catch(result => {
+//     console.log(`❌ Rejected promise ${result.position} in ${result.currentDelay}ms`);
+//   });
 
